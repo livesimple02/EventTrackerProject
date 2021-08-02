a5b6bae2d020347e49569c9df4f74977f365a709
livesimple02 <shlaupan@yahoo.com>
2021-08-02 09:46:51 -0600

    Modified to allow for deployment to EC2

--- TimeTrackerApp/src/main/java/com/skilldistillery/timetracker/TimeTrackerAppApplication.java
+++ TimeTrackerApp/src/main/java/com/skilldistillery/timetracker/TimeTrackerAppApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.timetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TimeTrackerAppApplication {
+public class TimeTrackerAppApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TimeTrackerAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimeTrackerAppApplication.class, args);
